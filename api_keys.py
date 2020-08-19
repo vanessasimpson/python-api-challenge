b1875ff614bdc666dd702b0546ee1618abab158b
Vanessa Simpson <66039717+vanessasimpson@users.noreply.github.com>
2020-08-18 22:16:14 -0500

    Added Final Outputs

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0753a7ffef79182f8913727b7ab6363a"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyChfEkdFZ81ezsUtgi7ZAb8O4tBG3E76wM"
